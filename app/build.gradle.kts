71523ef8092a2cc6cae1dc3322b5df4047683dec
bob <rjwhitmercc@gmail.com>
2026-06-16 16:25:49 -0600

    Upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.github.formula1"
         minSdk = 29
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.09"
+        versionCode = 10
+        versionName = "1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
